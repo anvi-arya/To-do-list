09f39bfb08d4f7e2845cfc83145547ff56c47a5c
anvi-arya <anviarya846@gmail.com>
2023-01-20 16:10:56 +0530

    version 0.1

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const createTask = document.querySelector("btn");
-createTask.addEventListener("click", display);
+const createTask = document.querySelector(".form-designn");
+const taskContainer = document.querySelector(".container");
+createTask.addEventListener("submit", display);
 
-function display() {
-  const task = document.getElementById(task).value;
-  const dispTask = document.createElement("input");
-  dispTask.textContent = task;
-  body.appendchild(dispTask);
+function display(event) {
+  event.preventDefault();
+  const task = document.getElementById("task-input");
+  const dispTask = document.createElement("div");
+  const text = document.createElement("span");
+  const cross = document.createElement("span");
+  dispTask.appendChild(text);
+  dispTask.appendChild(cross);
+  cross.textContent = "❌";
+  dispTask.classList.add("task-item");
+  if (task.value === "") return;
+  text.textContent = task.value;
+
+  text.onclick = (e) => {
+    console.log(e);
+    e.target.classList.add("completed");
+  };
+  cross.addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+    console.log(e);
+  });
+  task.value = "";
+  taskContainer.appendChild(dispTask);
 }
